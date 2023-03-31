471ca6e130a786aa61598c6f05abec11d2a16a41
inna9Z <vozniakinna@gmail.com>
2023-03-31 23:08:04 +0200

    git push --set-upstream origin home_page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,21 +1,13 @@
-
-import Menu from '../components/Home/Menu';
-import Navbar from '../components/Home/Navbar';
-import './styles/Home.css';
+import Menu from "../components/Home/Menu";
+import Navbar from "../components/Home/Navbar";
+import "./styles/Home.css";
 
 export const Home: React.FC = () => {
   return (
     <>
-    <Menu/>
-    <Navbar/>
-  
-     
-     
-     
-     
-   </>
+      <Menu />
+      <Navbar />
+    </>
   );
 };
 export default Home;
-
-
