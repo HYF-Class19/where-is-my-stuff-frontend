a9cb14563c6fda721008b805dd74976ca3f76fe5
anass abbar <aabaar@gmail.com>
2023-04-02 14:29:04 +0100

    added actionsheet

--- src/Modal/Details.tsx
+++ src/Modal/Details.tsx
@@ -1,6 +1,6 @@
-// src/components/ReminderDetailsModal.tsx
+import { ActionSheetToDeleteAndUpdate } from "./IonicActiunSheet";
 
-import React from "react";
+import React, { useState } from "react";
 import {
   IonModal,
   IonHeader,
@@ -17,6 +17,7 @@ import {
   IonCardContent,
   IonCardHeader,
   IonCardTitle,
+  IonActionSheet,
 } from "@ionic/react";
 import {
   chevronBackOutline,
@@ -24,7 +25,7 @@ import {
 } from "ionicons/icons";
 
 interface ReminderDetailsModalProps {
-  isOpen: boolean;
+  isOpen: any;
   onDismiss: () => void;
   itemName: string;
   description: string;
@@ -40,44 +41,109 @@ export const ReminderDetailsModal: React.FC<ReminderDetailsModalProps> = ({
   to,
   on,
 }) => {
+  const [isAction, setIsAction] = useState(false);
+  const [action, setAction] = useState("");
+
+  const handleActionSheet = (action: string) => {
+    setIsAction(true);
+    setAction(action);
+  };
+
+  const handleActionSheetDismiss = () => {
+    setIsAction(false);
+  };
+
+  function handleDeleteClick(): boolean | void | Promise<boolean | void> {
+    throw new Error("Function not implemented.");
+  }
+
+  function setShowUpdateModal(arg0: boolean) {
+    throw new Error("Function not implemented.");
+  }
+
   return (
-    <IonModal isOpen={isOpen} onDidDismiss={onDismiss}>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Details</IonTitle>
-          <IonButtons slot="start">
-            <IonIcon icon={chevronBackOutline} color="primary"></IonIcon>
-            <IonButton onClick={onDismiss}>Back</IonButton>
-          </IonButtons>
-          <IonButtons slot="end" >
-            <IonButton >
-              <IonIcon icon={ellipsisHorizontalOutline} color="primary"></IonIcon>
-            </IonButton>
-          </IonButtons>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent>
-      <IonCard style={{ margin: "0px" }}>
-          <IonCardHeader>
-            <IonCardTitle>{itemName}</IonCardTitle>
-          </IonCardHeader>
-          <IonCardContent>{description}</IonCardContent>
-        </IonCard>
-        <IonList>
-          <IonItem>
-            <IonLabel position="fixed">To</IonLabel>
-            <IonLabel>{to}</IonLabel>
-          </IonItem>
-          <IonItem>
-            <IonLabel position="fixed">On</IonLabel>
-            <IonLabel>{on}</IonLabel>
-          </IonItem>
-          <IonItem>
-            <IonLabel position="fixed">Reminder</IonLabel>
-            <IonLabel>{on}</IonLabel>
-          </IonItem>
-        </IonList>
-      </IonContent>
-    </IonModal>
+    <>
+      <IonModal isOpen={isOpen} onDidDismiss={onDismiss}>
+        <IonHeader>
+          <IonToolbar>
+            <IonButtons slot="start">
+              <IonButton onClick={onDismiss}>
+                <IonIcon
+                  icon={chevronBackOutline}
+                  slot="icon-only"
+                  color="primary"
+                />
+              </IonButton>
+            </IonButtons>
+            <IonTitle>Details</IonTitle>
+            <IonButtons slot="end">
+              <IonButton onClick={() => handleActionSheet("reminder")}>
+                <IonIcon
+                  icon={ellipsisHorizontalOutline}
+                  slot="icon-only"
+                  color="primary"
+                />
+              </IonButton>
+            </IonButtons>
+          </IonToolbar>
+        </IonHeader>
+        <IonContent>
+          <IonCard style={{ margin: "0px" }}>
+            <IonCardHeader>
+              <IonCardTitle>{itemName}</IonCardTitle>
+            </IonCardHeader>
+            <IonCardContent>{description}</IonCardContent>
+          </IonCard>
+          <IonList>
+            <IonItem>
+              <IonLabel position="fixed">To</IonLabel>
+              <IonLabel>{to}</IonLabel>
+            </IonItem>
+            <IonItem>
+              <IonLabel position="fixed">On</IonLabel>
+              <IonLabel>{on}</IonLabel>
+            </IonItem>
+          </IonList>
+        </IonContent>
+      </IonModal>
+      <IonActionSheet
+                isOpen={isAction}
+                onDidDismiss={onDismiss}
+                header={"You have ability to delete or update this " + action}
+                buttons={[
+                    {
+                        text: 'Delete',
+                        role: 'destructive',
+                        handler: handleDeleteClick,
+                        data: {
+                            action: 'delete'
+                        }
+                    },
+                    {
+                        text: 'Update',
+                        handler: () => {
+                            setShowUpdateModal(true);
+                           
+                        },
+                        data: {
+                            action: 'update'
+                        }
+                    },
+                    {
+                        text: 'Cancel',
+                        role: 'cancel',
+                        handler: () => {
+                            
+                        },
+                        data: {
+                            action: 'cancel'
+                        }
+                    }
+                ]}
+                cssClass='my-custom-class'
+                mode="ios"
+                translucent={true}
+            ></IonActionSheet>
+    </>
   );
 };
